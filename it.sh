ad3d8e7635a31b1a7134169d9ff4cc44bce78ffb
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-07-27 11:30:06 +0530

    Update it.sh

--- it.sh
+++ it.sh
@@ -2,6 +2,6 @@ echo "$1 uploading pixel"; \
 curl -T "$1" -u :"011ac321-1c21-4aa0-8608-ea69c55fa7ea" https://pixeldrain.com/api/file/; \
 echo "$1 uploaded pixel drain"; \
 echo "$1 UPLOADING onedrive"; \
-rclone copy /app/"$1" one:Public/July/"$2"; \
+rclone copy /app/catuserbot-master/"$1" one:Public/July/"$2"; \
 echo "$1 uploaded onedrive"; \
 echo "script complete"
