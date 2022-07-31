c7935fb4149847d84a3d380902c867873ed6a23a
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-07-31 11:50:08 +0530

    Update link.sh

--- link.sh
+++ link.sh
@@ -4,12 +4,18 @@ curl --silent -X GET "https://api.shareus.in/shortLink?token=F9SuUZRYdwNr2HU7316
 curl --silent -X GET "https://exe.io/api?api=c5a8ab7847458eb90585ce8e058a94371b7c9de2&url=$1&format=text" > out.txt
 curl --silent -X GET "https://exe.io/api?api=c5a8ab7847458eb90585ce8e058a94371b7c9de2&url=$2&format=text" > out1.txt
 curl --silent -X GET "https://exe.io/api?api=c5a8ab7847458eb90585ce8e058a94371b7c9de2&url=$3&format=text" > out2.txt
+echo External Links
+echo SCREENSHOTS 👉
+echo DOWNLOAD HERE👇👇👇
+echo Shareus link👇👇
 echo 480p👉: "$(jq -r '.shortlink' out.json)"
 echo 720p👉: "$(jq -r '.shortlink' out1.json)"
 echo 1080p👉: "$(jq -r '.shortlink' out2.json)"
 echo -----------------------------
-echo ""
+echo Exe.io Link👇👇👇
 echo 480p👉: `cat out.txt`
 echo 720p👉: `cat out1.txt`
 echo 1080p👉: `cat out2.txt`
+echo 
+echo HOW TO DOWNLOAD : https://t.me/screenshot18/2032
 rm *json *txt
