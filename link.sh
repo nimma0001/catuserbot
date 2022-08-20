5b1e742bdf69e3998bed2b54e716e99c014c0c3d
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-08-20 15:56:20 +0530

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,25 +1,16 @@
-curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$1&format=text
-" > out.txt
-curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$2&format=text
-" > out1.txt
-curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$3&format=text
-" > out2.txt
-curl --silent -X GET "https://rocklinks.net/api?api=a639c88768f207724337654a444156069734977a&url=$1&format=text" > out3.txt
-curl --silent -X GET "https://rocklinks.net/api?api=a639c88768f207724337654a444156069734977a&url=$2&format=text" > out4.txt
-curl --silent -X GET "https://rocklinks.net/api?api=a639c88768f207724337654a444156069734977a&url=$3&format=text" > out5.txt
+curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$1&format=text" > out.txt
+curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$2&format=text" > out1.txt
+curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$3&format=text" > out2.txt
 echo External Links
-echo SCREENSHOTS 👉
 echo DOWNLOAD HERE👇👇👇
-echo Bindass link👇👇
+echo Rocklink Link👇👇👇
 echo 480p👉: `cat out.txt`
 echo 720p👉: `cat out1.txt`
 echo 1080p👉: `cat out2.txt`
-echo -----------------------------
-echo Rocklink Link👇👇👇
-echo 480p👉: `cat out3.txt`
-echo 720p👉: `cat out4.txt`
-echo 1080p👉: `cat out5.txt`
-echo 
-echo HOW TO DOWNLOAD : https://t.me/screenshot18/2032
-echo
-rm *.txt
+echo ━━━━━━━━━━━━━━━━━
+echo 💁‍♀️How To Download👇
+echo 👉 https://t.me/screenshot18/2032
+echo ━━━━━━━━━━━━━━━━━
+echo 🔥𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥🔥
+echo 👉 https://t.me/+CY8KndRYiZM1NGI1
+rm *txt
