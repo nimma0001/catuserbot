af208a1966d57c622a228dd0f19d3ec50ae52b19
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-09-05 09:54:27 +0530

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
-curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$1&format=text" > out.txt
-curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$2&format=text" > out1.txt
-curl --silent -X GET "https://bindaaslinks.com/api?api=d27ce2b8647ce27448c17d54e74851f9751618ac&url=$3&format=text" > out2.txt
+curl --silent -X GET "https://rocklinks.net/api?api=a639c88768f207724337654a444156069734977a&url=$1&format=text" > out.txt
+curl --silent -X GET "https://rocklinks.net/api?api=a639c88768f207724337654a444156069734977a&url=$2&format=text" > out1.txt
+curl --silent -X GET "https://rocklinks.net/api?api=a639c88768f207724337654a444156069734977a&url=$3&format=text" > out2.txt
 echo External Links
 echo DOWNLOAD HERE👇👇👇
 echo Rocklink Link👇👇👇
@@ -9,7 +9,7 @@ echo 720p👉: `cat out1.txt`
 echo 1080p👉: `cat out2.txt`
 echo ━━━━━━━━━━━━━━━━━
 echo 💁‍♀️How To Download👇
-echo 👉 https://t.me/screenshot18/2032
+echo 👉 https://t.me/screenshot18/2772
 echo ━━━━━━━━━━━━━━━━━
 echo 🔥𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥🔥
 echo 👉 https://t.me/+CY8KndRYiZM1NGI1
